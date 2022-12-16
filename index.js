46c14eb1ea57e34de0d18edd397a17c9a79f2f93
Abir Hasan <cse.170201063@gmail.com>
2022-12-17 05:39:28 +0600

    test server side

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 1000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// env variable datas
+const user = process.env.DB_USER;
+const password = process.env.PASS;
+
+// mongodb and api
+const uri = `mongodb+srv://${user}:${password}@cluster0.nvx6pod.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+
+
+async function run() {
+  try {
+    const usersCollection = client.db('agtServer').collection('users');
+    app.post('/user', async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const result = await usersCollection.insertOne(user);
+      res.send(result)
+    })
+    
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("server running...");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
